fda05c50a29db65ff35a4dd936c4714db333fa89
Mochammad Farros Fatchur Roji <farros.alfatih@gmail.com>
2025-10-21 15:52:45 +0700

    up: katalog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  adapter: node({
+    mode: 'standalone'
+  }),
+  integrations: [tailwind()]
 });
